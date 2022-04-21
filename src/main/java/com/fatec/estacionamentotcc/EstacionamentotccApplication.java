14c4c5313a1dd2b923af0c8237eaad589b3e74c5
Lucas Campos <lucash_c@yahoo.com.br>
2022-04-21 15:29:51 -0300

    Projeto Criado

---
+++ src/main/java/com/fatec/estacionamentotcc/EstacionamentotccApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.estacionamentotcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstacionamentotccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstacionamentotccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatec/estacionamentotcc/EstacionamentotccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.estacionamentotcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstacionamentotccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
