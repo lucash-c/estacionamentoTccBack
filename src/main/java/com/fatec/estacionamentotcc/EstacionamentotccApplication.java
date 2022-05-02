19a61c434a2230b9e31da8685a79c6544b315644
Lucas Campos <lucash_c@yahoo.com.br>
2022-05-01 23:50:17 -0300

    operação de instanciação de usuario

--- src/main/java/com/fatec/estacionamentotcc/EstacionamentotccApplication.java
+++ src/main/java/com/fatec/estacionamentotcc/EstacionamentotccApplication.java
@@ -1,13 +1,32 @@
 package com.fatec.estacionamentotcc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.fatec.estacionamentotcc.domain.Usuario;
+import com.fatec.estacionamentotcc.repositories.UsuarioRepository;
+
 @SpringBootApplication
-public class EstacionamentotccApplication {
+public class EstacionamentotccApplication implements CommandLineRunner {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstacionamentotccApplication.class, args);
+	}
+
+	public void run(String... args) throws Exception {
+
+		Usuario user = new Usuario("Lucas", "assad44");
+		Usuario user2 = new Usuario("Joao", "sadds233");
+
+		usuarioRepository.saveAll(Arrays.asList(user, user2));
+
 	}
 
 }
